d49057a81aa416d2aa5ecf060b8246b4f4cb66c2
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-12-25 16:19:49 +0900

    ramen_shop model作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_20_043623) do
+ActiveRecord::Schema.define(version: 2021_12_25_071832) do
+
+  create_table "ramen_shops", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
