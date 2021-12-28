37bf74a67306b6f8caa50d232606c7e23083f717
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-12-28 18:49:05 +0900

    reviewモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_25_071832) do
+ActiveRecord::Schema.define(version: 2021_12_28_094751) do
 
   create_table "ramen_shops", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "ramen_shop_id", null: false
+    t.string "content"
+    t.integer "score"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ramen_shop_id"], name: "index_reviews_on_ramen_shop_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +42,6 @@ ActiveRecord::Schema.define(version: 2021_12_25_071832) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reviews", "ramen_shops"
+  add_foreign_key "reviews", "users"
 end
