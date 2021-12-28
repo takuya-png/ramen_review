cfeec2bd351ccbda3cf72e36b52be67d75ece79b
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-12-28 18:58:25 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
-  resources :ramen_shops, only: [:index, :show]
+  resources :ramen_shops, only: [:index, :show] do
+    resources :reviews, only: [:index, :create]
+  end
 end
