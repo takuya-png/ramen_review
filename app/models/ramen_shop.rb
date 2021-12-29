f3d226db6076a43f4ace49cd5ac51d600f591aa4
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-12-30 08:25:07 +0900

    fix

--- app/models/ramen_shop.rb
+++ app/models/ramen_shop.rb
@@ -7,6 +7,7 @@ class RamenShop < ApplicationRecord
       0.0
     end
   end
+  
   def review_score_percentage
     unless self.reviews.empty?
       reviews.average(:score).round(1).to_f * 100 / 5
