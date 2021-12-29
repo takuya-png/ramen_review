e2294247886b53d7b2a8c8ea36c0fbaca0ddd9df
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-12-29 22:31:49 +0900

    reviewsコントローラアクション追記

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,20 @@
 class ReviewsController < ApplicationController
   def index
   end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      redirect_to ramen_shop_reviews_path(@review.ramen_shop)
+    else
+      @ramen_shop = RamenShop.find(params[:id])
+      render "ramen_shops/show"
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:ramen_shop_id, :score, :content)
+  end
 end
